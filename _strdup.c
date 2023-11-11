38a704b27f3e8f702abd85fd735a49248375618e
Obed Kwartekwei Quartey <okquartey@st.ug.edu.gh>
2023-11-11 14:31:40 -0800

    _strdup.c

---
+++ _strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strdup - Duplicates a string.
+ * @str: string to duplicate
+ *
+ * Return: A pointer to the duplicated string, NULL if it fails/
+ */
+
+char *_strdup(const char *str)
+{
+	char *dup_str;
+	int i = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	dup_str = malloc(_strlen(str) + 1);
+
+	if (dup_str == NULL)
+		return (NULL);
+
+	while (str[i] != '\0')
+	{
+		dup_str[i] = str[i];
+		i++;
+	}
+	dup_str[i] = '\0';
+
+	return (dup_str);
+}
