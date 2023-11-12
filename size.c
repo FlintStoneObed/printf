f170daea462f5f4b92e775f1c13d7f419cfe21c5
Oscar Akwantey Boafo <141040248+regalconcepts@users.noreply.github.com>
2023-11-12 22:07:32 +0000

    size.c
    I updated the comment section of size.c

--- size.c
+++ size.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- *  * get_size - Calculates the size to cast the argument
- *   * @format: Formatted string in which to print the arguments
- *    * @i: List of arguments to be printed.
- *     *
- *      * Return: Precision.
+ * get_size - This calculates the size to cast the arg.
+ * @format: Formatted str in which to print the arg.
+ * @i: List of arg to be printed.
+ *
+ * Return: Precision.
  */
 
 int get_size(const char *format, int *i)
